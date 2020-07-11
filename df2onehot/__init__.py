baf7425f7b5ab66e320d171ecee9c80c172ad497
Erdogan Taskesen <erdogant@gmail.com>
2020-07-11 21:18:14 +0200

    new version

--- df2onehot/__init__.py
+++ df2onehot/__init__.py
@@ -11,7 +11,7 @@ from df2onehot.set_dtypes import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 # module level doc-string
 __doc__ = """
