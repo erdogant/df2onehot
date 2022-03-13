6ca7f66706b280a9ad70dcb0f6b71a9fb24f931e
erdogant <erdogant@gmail.com>
2022-03-13 14:31:34 +0100

    update examples

--- df2onehot/examples.py
+++ df2onehot/examples.py
@@ -1,30 +1,3 @@
-from df2onehot import df2onehot, import_example
-import pandas as pd
-import pypickle
-filepath = 'D://GITLAB/PROJECTS/shodanrws/results/shodandata.pkl'
-X = pypickle.load(filepath)
-df = pd.json_normalize(X['matches'])
-
-Xout = df2onehot(df, deep_extract=True, verbose=3)
-# Xext = df2onehot(df, deep_extract=deep_extract, y_min=1, perc_min_num=0.8, verbose=3)
-
-Xout['onehot'].shape
-Xout['onehot'].sum(axis=0)
-len(Xout['labx'])
-
-Xout['numeric'].shape
-len(Xout['labels'])
-
-Xout['df'].shape
-
-import sweetviz as sv
-my_report = sv.analyze(Xout['df'].iloc[:,0:100], pairwise_analysis='off')
-my_report.show_html() # Default arguments will generate to "SWEETVIZ_REPORT.html"
-
-# Doe iets met timestamp
-# Doe iets met locatie
-
-
 # %%
 import numpy as np
 from df2onehot import df2onehot, import_example
@@ -46,7 +19,7 @@ df = import_example(data="titanic")
 
 # %% Convert
 results = df2onehot(df)
-out['numeric']
+results['numeric']
 
 # %% Force feature (int or float) to be numeric if unique non-zero values are above percentage.
 out = df2onehot(df, perc_min_num=0.8)
